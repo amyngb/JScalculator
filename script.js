e8966e0ec2f3644587b7ad391f433f7ad6c0fc41
Amy Bedinghaus <amyngbedinghaus@gmail.com>
2017-10-22 07:27:23 -0400

    add screen history display

--- script.js
+++ script.js
@@ -9,23 +9,20 @@ $(document).ready(function (){
     reset();
 
     function reset () {
-        $("#screen").html("0");
+        $("#screen-num").html("0");
     }
 
     function displayCurr(curr){
         //read screen
-        var temp = $("#screen").html();
-        temp.split("");
-        
-        
-        console.log(typeof(temp));
+        var temp = $("#screen-num").html();
+    
         if (temp == 0) {
-            $("#screen").html(curr);
+            $("#screen-num").html(curr);
         }
 
-        else if (temp > 0 && temp.length < 8) {
-            temp = temp.push(curr);
-            $("#screen").html(temp);
+        else if (temp > 0 && temp.length < 7) {
+            temp = temp.concat(curr);
+            $("#screen-num").html(temp);
         }
         else {
             //say the digit limit has been reached, reset();
@@ -36,69 +33,69 @@ $(document).ready(function (){
 
     //0 
     $("#k0").click(function(){
-        curr = 0;
+        curr = "0";
         displayCurr(curr);
     });
 
     //1
     $("#k1").click(function(){
-        curr = 1;
+        curr = "1";
         console.log(curr);
         displayCurr(curr);
     });
 
     //2
     $("#k2").click(function(){
-        curr = 2;
+        curr = "2";
         displayCurr(curr);
     });
 
     //3
     $("#k3").click(function(){
-        curr = 3;
+        curr = "3";
         displayCurr(curr);
     });
 
     //4
     $("#k4").click(function(){
-        curr = 4;
+        curr = "4";
         displayCurr(curr);
     });
 
     //5
     $("#k5").click(function(){
-        curr = 5;
+        curr = "5";
         displayCurr(curr);
     });
 
     //6
     $("#k6").click(function(){
-        curr = 6;
+        curr = "6";
         displayCurr(curr);
     });
 
     //7
     $("#k7").click(function(){
-        curr = 7;
+        curr = "7";
         displayCurr(curr);
 
     });
 
     //8
     $("#k8").click(function(){
-        curr = 8;
+        curr = "8";
         displayCurr(curr);
     });
 
     //9
     $("#k9").click(function(){
-        curr = 9;
+        curr = "9";
         displayCurr(curr);
     });
 
     //CE
     $("#CE").click(function(){
-        curr = 0;
+        curr = "0";
         displayCurr(curr);
     });
 
